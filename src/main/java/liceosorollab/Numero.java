c6c55eb85fd57f2baa83c8d8a742b6025c1f7104
DAM_1 <DAM_1@PB03>
2020-03-06 20:40:03 +0100

    Comprobacion numero1 y numero2

--- src/main/java/liceosorollab/Numero.java
+++ src/main/java/liceosorollab/Numero.java
@@ -1,5 +1,36 @@
 package main.java.liceosorollab;
 
 public class Numero {
+	
+	private int numero1;
+	private int numero2;
+	
+	public Numero(int numero1, int numero2) {
+		
+		this.numero1 = numero1;
+		this.numero2 = numero2;
+	}
 
+	public int getNumero1() {
+		return numero1;
+	}
+	
+	public void setNumero1(int numero1) {
+		this.numero1 = numero1;
+	}
+	
+	public int getNumero2() {
+		return numero2;
+	}
+	
+	public void setNumero2(int numero2) {
+		this.numero2 = numero2;
+	}
+
+	public String toString() {
+		return "numero1 " + numero1 + ", numero2 " + numero2;
+	}
+
+
+	
 }
--- src/test/java/liceosorollab/NumeroTest.java
+++ src/test/java/liceosorollab/NumeroTest.java
@@ -1,5 +1,32 @@
 package test.java.liceosorollab;
 
-public class NumeroTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorollab.Numero;
+
+
+public class NumeroTest {
+	
+	private Numero numero;
+
+	@Before
+	public void before() {
+		numero = new Numero(6,10);
+	}
+		
+	@Test
+	public void comprobarNumero1() {
+		
+		assertEquals(6,numero.getNumero1());
+	}
+	
+	@Test
+	public void comprobarNumero2() {
+		
+		assertEquals(10,numero.getNumero1());
+	}
+	
 }
