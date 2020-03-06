30bd85bca8ccb8dd47cead7ef91172af6b7a6e08
DAM_1 <DAM_1@PB05>
2020-03-06 19:12:40 +0100

    Rectangulo

--- src/main/java/liceosorollab/Rectangulo.java
+++ src/main/java/liceosorollab/Rectangulo.java
@@ -1,5 +1,21 @@
 package main.java.liceosorollab;
 
 public class Rectangulo {
+	
+	private int lado1;
+	private int lado2;
+	
+	public Rectangulo(int lado1,int lado2) {
+		this.lado1 = lado1;
+		this.lado2 = lado2;
+	}
 
+	public boolean cuadrado() {
+		boolean cuadrado = false;
+		if(this.lado1 == this.lado2) {
+			cuadrado = true;
+		};
+		return cuadrado;
+	}
 }
+
--- src/test/java/liceosorollab/RectanguloTest.java
+++ src/test/java/liceosorollab/RectanguloTest.java
@@ -1,5 +1,23 @@
 package test.java.liceosorollab;
 
-public class RectanguloTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import main.java.liceosorollab.Rectangulo;
+
+public class RectanguloTest {
+	
+	private Rectangulo rectangulo;
+
+	@Before
+	public void before() {
+		rectangulo = new Rectangulo(5,5);
+	}
+	@Test
+	public void compruebaHoras() {
+		assertEquals(true,rectangulo.cuadrado());
+	}
+	
 }
