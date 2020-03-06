daa865be87211b3fe84dbce48478ef9e8d66c6f3
alexs <alexs@LAPTOP-KQGHEVBO>
2020-03-06 23:47:32 +0100

    usuario

--- src/test/java/liceosorollab/UsuarioTest.java
+++ src/test/java/liceosorollab/UsuarioTest.java
@@ -1,5 +1,63 @@
 package test.java.liceosorollab;
 
-public class UsuarioTest {
+import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDate;
+
+import org.junit.Test;
+import org.junit.Before;
+import main.java.liceosorollab.Usuario;
+
+public class UsuarioTest {
+	private Usuario usuario;
+
+	@Before
+	public void Before() {
+
+		usuario = new Usuario("Alejandro","	Sanchez Fernandez", LocalDate.of(2000, 06, 10), 6);
+	}
+
+	@Test
+
+	public void comprobarNombre() {
+
+		assertEquals("Alejandro", usuario.getNombre());
+	}
+
+	@Test
+
+	public void comprobarApellidos() {
+
+		assertEquals("Sanchez Fernandez", usuario.getApellidos());
+	}
+
+	@Test
+
+	public void comprobarNombreCompleto() {
+
+		assertEquals("Alejandro Sanchez Fernandez ", usuario.getNombre()+ " "+usuario.getApellidos());
+	}
+
+	@Test
+
+	public void comprobarMayoriaDeEdad() {
+
+		assertEquals(true,usuario.mayoriaEdad());
+	}
+
+	@Test
+
+	public void comprobarIdentificador() {
+		assertEquals(6,usuario.getIdentificador());
+	}
+
+	@Test
+	public void comprobarNombreMayusculas() {
+		assertEquals("ALEJANDRO",usuario.getNombre().toUpperCase());
+	}
+
+	@Test
+	public void comprobarNombreMinusculas() {
+		assertEquals("alejandro",usuario.getNombre().toLowerCase());
+	}
 }
